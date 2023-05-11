83a4102a17ddc4350612cc6520b3788949d45116
Didit6 <88450146+Didit6@users.noreply.github.com>
2023-05-11 13:42:28 +0700

    Create tabwksna.js

---
+++ tauwvsna.js
@@ -0,0 +1,346 @@
+// Create grid container A
+var cnms = document.getElementById("tekschrd");
+var gbr1 = document.getElementById("ig1");
+var gbr2 = document.getElementById("ig2");
+var gbr3 = document.getElementById("ig3");
+var gbr4 = document.getElementById("ig4");
+var gidc1 = document.createElement('div');
+gidc1.setAttribute("id","grd1");
+document.getElementById("DIV1").appendChild(gidc1);
+for(a=0;a<47;a++){
+var nmsx1 = ["A","A/C#","A/E","A/F","A11","A13","A6","A69","A7","A7#9","A7b5","A7b9","A7sus4","A9","A9#11","A9b5","Aadd9","Aalt","Aaug","Aaug7","Aaug9","Adim","Adim7","Am","Am/C","Am/E","Am/F","Am/F#","Am/G","Am11","Am6","Am69","Am7","Am7b5","Am9","Amadd9","Amaj11","Amaj13","Amaj7","Amaj7#5","Amaj7b5","Amaj9","AmMaj11","AmMaj7","AmMaj9","Asus2","Asus4"];
+var btnx1 = document.createElement("div");
+btnx1.innerHTML = nmsx1[a];
+btnx1.setAttribute("class","koloms1");
+document.getElementById("grd1").appendChild(btnx1);
+}
+var tmbl1 = document.getElementsByClassName("koloms1");
+tmbl1[0].onclick = function(){
+	cnms.innerHTML = nmsx1[0];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[1].onclick = function(){
+	cnms.innerHTML = nmsx1[1];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[2].onclick = function(){
+	cnms.innerHTML = nmsx1[2];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[3].onclick = function(){
+	cnms.innerHTML = nmsx1[3];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[4].onclick = function(){
+	cnms.innerHTML = nmsx1[4];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[5].onclick = function(){
+	cnms.innerHTML = nmsx1[5];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[6].onclick = function(){
+	cnms.innerHTML = nmsx1[6];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[7].onclick = function(){
+	cnms.innerHTML = nmsx1[7];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[8].onclick = function(){
+	cnms.innerHTML = nmsx1[8];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[9].onclick = function(){
+	cnms.innerHTML = nmsx1[9];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[10].onclick = function(){
+	cnms.innerHTML = nmsx1[10];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[11].onclick = function(){
+	cnms.innerHTML = nmsx1[11];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[12].onclick = function(){
+	cnms.innerHTML = nmsx1[12];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[13].onclick = function(){
+	cnms.innerHTML = nmsx1[13];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[14].onclick = function(){
+	cnms.innerHTML = nmsx1[14];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[15].onclick = function(){
+	cnms.innerHTML = nmsx1[15];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[16].onclick = function(){
+	cnms.innerHTML = nmsx1[16];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[17].onclick = function(){
+	cnms.innerHTML = nmsx1[17];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[18].onclick = function(){
+	cnms.innerHTML = nmsx1[18];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[19].onclick = function(){
+	cnms.innerHTML = nmsx1[19];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[20].onclick = function(){
+	cnms.innerHTML = nmsx1[20];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[21].onclick = function(){
+	cnms.innerHTML = nmsx1[21];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[22].onclick = function(){
+	cnms.innerHTML = nmsx1[22];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[23].onclick = function(){
+	cnms.innerHTML = nmsx1[23];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[24].onclick = function(){
+	cnms.innerHTML = nmsx1[24];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[25].onclick = function(){
+	cnms.innerHTML = nmsx1[25];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[26].onclick = function(){
+	cnms.innerHTML = nmsx1[26];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[27].onclick = function(){
+	cnms.innerHTML = nmsx1[27];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[28].onclick = function(){
+	cnms.innerHTML = nmsx1[28];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[29].onclick = function(){
+	cnms.innerHTML = nmsx1[29];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[30].onclick = function(){
+	cnms.innerHTML = nmsx1[30];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[31].onclick = function(){
+	cnms.innerHTML = nmsx1[31];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[32].onclick = function(){
+	cnms.innerHTML = nmsx1[32];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[33].onclick = function(){
+	cnms.innerHTML = nmsx1[33];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[34].onclick = function(){
+	cnms.innerHTML = nmsx1[34];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[35].onclick = function(){
+	cnms.innerHTML = nmsx1[35];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[36].onclick = function(){
+	cnms.innerHTML = nmsx1[36];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[37].onclick = function(){
+	cnms.innerHTML = nmsx1[37];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[38].onclick = function(){
+	cnms.innerHTML = nmsx1[38];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[39].onclick = function(){
+	cnms.innerHTML = nmsx1[39];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[40].onclick = function(){
+	cnms.innerHTML = nmsx1[40];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[41].onclick = function(){
+	cnms.innerHTML = nmsx1[41];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[42].onclick = function(){
+	cnms.innerHTML = nmsx1[42];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[43].onclick = function(){
+	cnms.innerHTML = nmsx1[43];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[44].onclick = function(){
+	cnms.innerHTML = nmsx1[44];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[45].onclick = function(){
+	cnms.innerHTML = nmsx1[45];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
+tmbl1[46].onclick = function(){
+	cnms.innerHTML = nmsx1[46];
+	gbr1.src = "xxxx";
+	gbr2.src = "xxxx";
+	gbr3.src = "xxxx";
+	gbr4.src = "xxxx";
+}
