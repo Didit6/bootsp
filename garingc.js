2716197a2b7a6e0b4da7f0f495bc4c81e42c0878
Didit6 <88450146+Didit6@users.noreply.github.com>
2024-01-19 13:05:36 +0700

    Create garingc.js

---
+++ garingc.js
@@ -0,0 +1,14 @@
+var clk = 0;var clkc = 0;
+function myL(elm) {
+   elm.clk = (elm.clk || 0) + 1;
+if(elm.clk == 1) {window.open("https://www.toprevenuegate.com/d1hc9ehmg9?key=06ad3ea188f394b294e9ce8c07815c73", "_blank");}else if(elm.clk == 2) {window.open("https://zirdough.net/4/5979366", "_blank");}else if(elm.clk == 3) {
+elm.innerHTML = '<i class = "fa fa-download" ></i>';
+document.getElementById("b1").onclick = function() {window.location.href = "https://drive.google.com/file/d/16wcylxXCbVruURN3awUWOc1-dKzK2gbt/view?usp=drive_link";}
+document.getElementById("b2").onclick = function() {window.location.href = "https://drive.google.com/file/d/1Q_oEnmsPAuHK1Icd_WP3r1WXv80tFb_5/view?usp=drive_link";}
+document.getElementById("b3").onclick = function() {window.location.href = "https://drive.google.com/file/d/1KVVeUgHMAbnxOK9wImEUvakeUt7or3kC/view?usp=drive_link";}
+document.getElementById("b4").onclick = function() {window.location.href = "https://drive.google.com/file/d/1YVk1NF8UmggVMy3Vl2-tuoQYEboi0Ppj/view?usp=drive_link";}
+document.getElementById("b5").onclick = function() {window.location.href = "https://drive.google.com/file/d/1kzsb5G6fBTnRL0jgthXxLtfOVhmsy2Hj/view?usp=drive_link";}
+document.getElementById("b6").onclick = function() {window.location.href = "https://drive.google.com/file/d/1KbI1YHgSwR4MioqtK_Gv7SbkCJwQLKch/view?usp=drive_link";}
+document.getElementById("b7").onclick = function() {window.location.href = "https://drive.google.com/file/d/1ADY5FfSeZRZG8egXUZTShSGfsSicOElH/view?usp=drive_link";}
+document.getElementById("b8").onclick = function() {window.location.href = "https://drive.google.com/file/d/1euLsBCagAsTPrKPMie3uePF0pcEvA9Lf/view?usp=drive_link";}
+}}
